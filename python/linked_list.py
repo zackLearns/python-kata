0a7e29c1a88c4b4e0299f6e45a2b44241c74a149
Zack <zack.learns@gmail.com>
2020-09-27 14:04:02 +0800

    modified clarity for add methods, added standard methods such as get by index and add by index

--- python/linked_list.py
+++ python/linked_list.py
@@ -1,33 +1,30 @@
 class Node:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
 
-    def __repr__(self):
+    def __init__(self, value):
+        self.next = None
+        self.value = value
+
+    def __str__(self):
         return f'{self.value}'
 
 
 class LinkedList:
-    def __init__(self, initial_values=None):
-        if initial_values is None:
-            self.head = None
-        else:
-            for value in initial_values:
-                self.add_last(value)
 
-    def __repr__(self):
-        node = self.head
-        nodes = []
-        while node is not None:
-            nodes.append(node.value)
-            node = node.next
-        nodes.append("None")
-        return " -> ".join(nodes)
+    def __init__(self):
+        self.size = 0
+        self.head = None
+
+    def __setitem__(self, index, value):
+        self.add_to_index(index, value)
+
+    def __getitem__(self, index):
+        return self.get(index)
 
     def add_first(self, value):
         new_node = Node(value)
         new_node.next = self.head
         self.head = new_node
+        self.size += 1
 
     def add_last(self, value):
         new_node = Node(value)
@@ -38,6 +35,27 @@ class LinkedList:
         while current_node.next is not None:
             current_node = current_node.next
         current_node.next = new_node
+        self.size += 1
+
+    def add_to_index(self, index, value):
+        new_node = Node(value)
+        if index >= self.size:
+            raise ValueError(f'Index passed in ({index}) is larger than the LinkedList size ({self.size}).')
+        if index == 0:
+            new_node.next = self.head
+            self.head = new_node
+        previous_node = self.head
+        current_index = 1
+        target_node = self.head.next
+        while target_node.next is not None:
+            if current_index == index:
+                previous_node.next = new_node
+                new_node.next = target_node
+                self.size += 1
+                return
+            current_index += 1
+            previous_node = target_node
+            target_node = target_node.next
 
     def add_after(self, target_value, value):
         new_node = Node(value)
@@ -45,60 +63,89 @@ class LinkedList:
             raise Exception('LinkedList is empty and cannot perform add_after function.')
 
         current_node = self.head
-        while current_node is not None:
+        while current_node.next is not None:
             if current_node.value == target_value:
                 new_node.next = current_node.next
                 current_node.next = new_node
+                self.size += 1
                 return
             current_node = current_node.next
-        raise ValueError('No node matches target_node passed in.')
+        raise ValueError(f'No node with value matching target_value {target_value} passed in.')
 
     def add_before(self, target_value, value):
-        if self.head is None:
-            raise Exception('LinkedList does not contain target_node passed in for add_after function.')
         new_node = Node(value)
+        if self.head is None:
+            raise Exception('LinkedList is empty and cannot perform add_before function.')
+
         previous_node = None
         current_node = self.head
-        while current_node is not None:
+        while current_node.next is not None:
             if current_node.value == target_value:
-                new_node.next = previous_node.next
                 previous_node.next = new_node
+                new_node.next = current_node
+                self.size += 1
                 return
             previous_node = current_node
             current_node = current_node.next
-        raise ValueError('LinkedList does not contain target_node passed in for add_before function.')
+        raise ValueError(f'No node with matching target_value {target_value} passed in.')
+
+    def get(self, index):
+        if index >= self.size:
+            raise Exception(f"Index {index} passed in is larger than the LinkedList's size")
+        if self.head is None:
+            raise Exception(f"LinkedList is empty and cannot retrieve index {index}")
+        current_index = 0
+        current_node = self.head
+        while current_node is not None:
+            if index == current_index:
+                return current_node
+            current_index += 1
+            current_node = current_node.next
 
     def remove(self, value):
         if self.head is None:
-            raise Exception('LinkedList is empty and cannot perform remove_node function.')
+            raise Exception('LinkedList is empty and cannot perform remove function.')
+        if self.head.value == value:
+            self.head = self.head.next
+            return
 
         previous_node = None
         current_node = self.head
-        while current_node is not None:
+        while current_node.next is not None:
             if current_node.value == value:
                 previous_node.next = current_node.next
+                self.size -= 1
                 return
             previous_node = current_node
             current_node = current_node.next
+        raise ValueError(f'No node with matching value {value} found.')
 
     def __str__(self):
-        to_return = '['
-        current_node = self.head
-        while current_node is not None:
-            to_return += f'{current_node.value}'
-            current_node = current_node.next
-            to_return += ', ' if current_node is not None else ''
-        to_return += ']'
+        if self.head is None:
+            return ''
+        to_return = f'{self.head} '
+        node = self.head
+        while node.next is not None:
+            node = node.next
+            to_return += f'{node} '
         return to_return
 
 
 if __name__ == '__main__':
-    linked_list = LinkedList()
-    linked_list.add_last(5)
-    linked_list.add_last(9)
-    linked_list.add_last(0)
-    linked_list.add_last(-9)
-    linked_list.add_last(89)
-    print(linked_list)
-    linked_list.remove(0)
-    print(linked_list)
+    numbers = LinkedList()
+    numbers.add_first(5)
+    numbers.add_first(10)
+    numbers.add_last(15)
+    print(f"{numbers}\n")  # 10, 5, 15
+
+    numbers.add_before(5, 8)
+    numbers.add_after(8, 12)
+    print(f"{numbers}\n")  # 10, 8, 12, 5, 15
+
+    numbers.remove(12)
+    numbers.remove(10)
+    print(f"{numbers}\n")  # 8, 5, 15
+
+    numbers[1] = 20
+    print(f"{numbers}\n")  # 8, 20, 5, 15
+    print(numbers[1])  # 20
